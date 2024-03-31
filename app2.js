c24ac52d509f96e7d8566dc9ddffc2a776e8bb86
SeferliGuler <seferliglr23@gmail.com>
2024-03-31 17:36:23 +0400

    music

---
+++ app2.js
@@ -0,0 +1,20 @@
+let songs = [
+    { 
+        name:'song 1',
+        path:'mp/1.mp3',
+        artist:'artist 1',
+        cover:'img/1.jpeg'
+    },
+    {
+        name:'song 2',
+        path:'mp/2.mp3',
+        artist:'artist 2',
+        cover:'img/2.jpeg'
+    },
+    {
+        name:'song 3',
+        path:'mp/3.mp3',
+        artist:'artist 3',
+        cover:'img/3.jpeg'
+    }
+ ]
